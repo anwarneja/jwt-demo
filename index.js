194c2e278ae220e039bf294ae9b216849e16e4dc
anwarneja <anwarneja55@gmail.com>
2025-09-18 14:53:55 +0300

    express.json

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
+const  jwt = require('jsonwebtoken');
+
+app.use(express.json());
 
 
-
-app.get('/jwt', authRoutes);
+app.use('/jwt', authRoutes);
 
 
 app.listen(3000,()=>{
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,32 @@
 const express = require('express');
-
+const app = express();
 const router = express.Router();
+
+
+
+const posts=[
+    {title:"quran",
+    content:"holy book"
+    }
+,
+    {title:"kitab",
+    content:"sunnah book"
+}
+]
+
+router.get('/post',(req,res)=>{
+   res.json(posts);
+   console.log(req.body);
+})
+
+router.post('/login',(req,res)=>{
+    //authenticate user
+    const user={id:1,username:"user1",email:"a@b.com"}
+    res.json(user);
+
+
+})
+
 
 
 module.exports = router;
