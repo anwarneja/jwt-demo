b226cb0e396525addd008f53404fe47c5a3e48cb
anwarneja <anwarneja55@gmail.com>
2025-09-20 23:50:18 +0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,4 +9,17 @@
 exports.verifytoken=(req,res,next)=>{
     const authheader=req.headers['authorization'];
     const token=authheader && authheader.split('')[0];
+
+
+    if(token==null){
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token,process.env.ACCESTOKEN,(err,user)=>{
+        if(err){
+            return res.sendStatus(403);
+        }
+        req.user=user;
+        next();
+    })
 }
