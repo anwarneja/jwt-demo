54ff7712c6616d1fdf02867db59d4f6ba4f95ced
anwarneja <anwarneja55@gmail.com>
2025-09-23 11:58:47 +0300

    fixed  the split in jwt and added filter to filter names

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,33 +1,30 @@
-const express = require('express');
-const router=express.Router();
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-exports.loginruth =async (req, res) =>{
-    //authenticate user
-const {name,age}=req.body;
+exports.loginruth = async (req, res) => {
+  //authenticate user
+  const { name, age } = req.body;
 
-const user={name:name,
-    age
-} ;
+  const user = { name: name, userage: age };
 
-const accestoken=jwt.sign(user,
-    process.env.ACCESTOKEN,
-   { expiresIn:'2h'} 
+  const accestoken = jwt.sign(user, process.env.ACCESTOKEN, {
+    expiresIn: "2h",
+  });
+  res.json({ accestoken: accestoken, user });
+};
 
-   )
-   res.json({accestoken:accestoken,
-               user})
-}
- 
+exports.post = async (req, res) => {
+  const post = [
+    {
+      name: "anwar",
+      content: "holy book",
+    },
+    { name: "ilham", content: "sunnah book" },
+    { name: "fatima", content: "hadith notes" },
+  ];
+  const userposts=post.filter(p=>p.name===req.user.name)
 
-exports.post=async(req,res)=>{
-     const post=[
-        {
-        name:"anwar",
-        content:"holy book"
-        }
-     ]
-    res.json({post,
-        "loogedinuser":req.user
-    });
-}
+  res.json({ post:userposts,//optional
+     loogedinuser: req.user });
+};
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,20 @@
 // const verifytoken=(req,res,next)=>{
 
-
-//     // BEARER TOKEN 
+//     // BEARER TOKEN
 // }
 
 // module.exports=verifytoken;
+const jwt = require('jsonwebtoken');
 
-exports.verifytoken=(req,res,next)=>{
-    const authheader=req.headers['authorization'];
-    const  token=authheader && authheader.split('')[0];
+exports.verifytoken = (req, res, next) => {
+  const authheader = req.headers["authorization"];
+  const token = authheader && authheader.split(" ")[1];
 
+  if (!token) return res.sendStatus(401); //unauthorized
 
-    if(!token)return res.sendStatus(401); //unauthorized
-
-    jwt.verify(token,process.env.ACCESTOKEN,(err,decoded));
-    if(err) return res.sendStatus(403); // Forbidden if invalid
-req.user=decoded;
-next();
-
-
-
-
-    
-}
+  jwt.verify(token, process.env.ACCESTOKEN, (err, decoded) => {
+    if (err) return res.sendStatus(403); // Forbidden if invalid
+    req.user = decoded;
+    next();
+  });
+};
